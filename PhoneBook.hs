ec6fa71ca559c69277589513333a07ce677ce8dc
Hongzhi Song <dldz2002@gmail.com>
2013-09-20 21:34:11 +0800

    Add some files.

---
+++ PhoneBook.hs
@@ -0,0 +1,7 @@
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
