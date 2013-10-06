a9016217c2c1a5095a84895eb227636859fb34d2
Hongzhi Song <dldz2002@gmail.com>
2013-10-06 16:51:06 +0800

    Improve optimal path algorithm.

--- heathrow.hs
+++ heathrow.hs
@@ -22,27 +22,25 @@ heathrowToLondon :: RoadSystem
 heathrowToLondon = [ Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
 
 
-roadStep :: (Path, Path) -> Section -> (Path, Path)
-roadStep (pathA, pathB) (Section a b c) =
-    let priceA = sum $ map snd pathA
-        priceB = sum $ map snd pathB
-        forwardPriceToA = priceA + a
-        crossPriceToA = priceB + b + c
-        forwardPriceToB = priceB + b
-        crossPriceToB = priceA + a + c
-        newPathToA = if forwardPriceToA <= crossPriceToA
-                        then (A,a):pathA
-                        else (C,c):(B,b):pathB
-        newPathToB = if forwardPriceToB <= crossPriceToB
-                        then (B,b):pathB
-                        else (C,c):(A,a):pathA
-    in (newPathToA, newPathToB)
+roadStep :: (Path, Path, Int, Int) -> Section -> (Path, Path, Int, Int)
+roadStep (pathA, pathB, bestPriceA, bestPriceB) (Section a b c) =
+    let forwardPriceToA = bestPriceA + a
+        crossPriceToA = bestPriceB + b + c
+        forwardPriceToB = bestPriceB + b
+        crossPriceToB = bestPriceA + a + c
+        (newPathToA, newBestPriceA) = if forwardPriceToA <= crossPriceToA
+                        then ((A,a):pathA,       forwardPriceToA)
+                        else ((C,c):(B,b):pathB, crossPriceToA)
+        (newPathToB, newBestPriceB) = if forwardPriceToB <= crossPriceToB
+                        then ((B,b):pathB,       forwardPriceToB)
+                        else ((C,c):(A,a):pathA, crossPriceToB)
+    in (newPathToA, newPathToB, newBestPriceA, newBestPriceB)
 
 
 optimalPath :: RoadSystem -> Path
 optimalPath roadSystem =
-    let (bestAPath, bestBPath) = foldl roadStep ([],[]) roadSystem
-    in if sum (map snd bestAPath) <= sum (map snd bestBPath)
+    let (bestAPath, bestBPath, bestAPrice, bestBPrice) = foldl roadStep ([],[], 0, 0) roadSystem
+    in if bestAPrice <= bestBPrice
         then reverse bestAPath
         else reverse bestBPath
 
