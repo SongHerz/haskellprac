9463772707093f30de8e524d2b87cfa17ab74e2d
Hongzhi Song <dldz2002@gmail.com>
2013-12-01 13:40:30 +0800

    Add another version of gcd.

--- gcdlog.hs
+++ gcdlog.hs
@@ -1,4 +1,5 @@
 import Control.Monad.Writer
+import DiffList
 
 gcd' :: Int -> Int -> Writer [String] Int
 gcd' a b
@@ -8,3 +9,14 @@ gcd' a b
     | otherwise = do
         tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
         gcd' b (a `mod` b)
+
+
+gcd'' :: Int -> Int -> Writer (DiffList String) Int
+gcd'' a b
+    | b == 0    = do
+        tell $ toDiffList ["Finished with " ++ show a]
+        return a
+    | otherwise = do
+        result <- gcd'' b (a `mod` b)
+        tell $ toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+        return result
