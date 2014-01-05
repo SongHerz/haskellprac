dd8dbd90343091837f45a39f91a6623725636b75
Hongzhi Song <dldz2002@gmail.com>
2014-01-05 13:52:36 +0800

    Some refactors

--- tightwalker2.hs
+++ tightwalker2.hs
@@ -11,19 +11,16 @@ errMsg (oldLeft, oldRight) (newLeft, newRight) =
     ++ " birds on the " ++ ( if newLeft > oldLeft then "left" else "right") ++ " make the man fall."
 
 {-
+ -- This does not work
 landLeft :: Error e => Birds -> Pole -> Either e Pole
 landLeft n (left, right)
     | abs ((left + n) - right) < 4 = Right (left + n, right)
-    | otherwise                    = Left $ strMsg $ "There are " ++ (show left) ++ "birds on the left, "
-                                            ++ (show right) ++ "birds on the right."
-                                            ++ "another " ++ (show n) ++ "birds on the left make the man fall."
+    | otherwise                    = Left $ strMsg $ errMsg (left, right) (left + n, right)
 
 landRight :: Error e => Birds -> Pole -> Either e Pole
 landRight n (left, right)
     | abs (left - (right + n)) < 4 = Right (left, right + n)
-    | otherwise                    = Left $ strMsg $ "There are " ++ (show left) ++ "birds on the left, "
-                                            ++ (show right) ++ "birds on the right."
-                                            ++ "another " ++ (show n) ++ "birds on the right make the man fall."
+    | otherwise                    = Left $ strMsg $ errMsg (left, right) (left, right + n)
 -}
 
 
