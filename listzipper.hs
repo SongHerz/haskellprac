93f842e6ea0640babfd4434fa55f83f8f6e0b508
Hongzhi Song <dldz2002@gmail.com>
2014-02-03 12:21:49 +0800

    Add listzipper

---
+++ listzipper.hs
@@ -0,0 +1,9 @@
+-- 1st list is the list we are focusing on
+-- 2nd list is the list of breadcrumbs
+type ListZipper a = ([a], [a])
+
+goForward :: ListZipper a -> ListZipper a
+goForward (x:xs, bs) = (xs, x:bs)
+
+goBack :: ListZipper a -> ListZipper a
+goBack (xs, b:bs) = (b:xs, bs)
