989a05e7c18219562659f880619188f991575f01
Hongzhi Song <dldz2002@gmail.com>
2014-02-03 23:26:53 +0800

    Add file to try HUnit

---
+++ hunit/FindIdentifier.hs
@@ -0,0 +1,5 @@
+module FindIdentifier ( findIdentifier) where
+
+findIdentifier :: String -> (Int, Int) -> Maybe String
+findIdentifier _ _ = Nothing -- TODO This is not yet the desired functionality
+
---
+++ hunit/FindIdentifier_Test.hs
@@ -0,0 +1,21 @@
+module FindIdentifier_Test where
+
+import FindIdentifier ( findIdentifier)
+import Test.HUnit
+
+borderCases = TestLabel "Border test cases" $ TestList [testEmpty, testNegCursor, testComment]
+
+testEmpty = TestCase $ assertEqual
+    "Should get Nothing from an empty string" Nothing ( findIdentifier "" (1,1))
+testNegCursor = TestCase $ assertEqual
+    "Should get Nothing when cursor is negative" Nothing ( findIdentifier "a" (-1,-1))
+testComment = TestCase $ assertEqual
+    "Should get Nothing on comment" Nothing ( findIdentifier "-- a" (1,3))
+
+
+simpleCases = TestLabel "Simple, but serious cases" $ TestList [ testMinimal]
+
+testMinimal = TestCase $ assertEqual
+    "Minimal program" (Just "main") ( findIdentifier "main = print 42" (1,2))
+
+main = runTestTT $ TestList [ borderCases, simpleCases]
