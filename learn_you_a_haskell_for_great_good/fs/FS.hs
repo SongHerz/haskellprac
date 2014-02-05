16cfb929a615cec45c08e4c27a28645caca54cef
Hongzhi Song <dldz2002@gmail.com>
2014-02-05 18:00:13 +0800

    Forgot to add this file

--- learn_you_a_haskell_for_great_good/fs/FS.hs
+++ learn_you_a_haskell_for_great_good/fs/FS.hs
@@ -14,28 +14,6 @@ type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
 
-myDisk :: FSItem
-myDisk =
-    Folder "Root"
-        [ File "goat_yelling_like_man.wmv" "baaaaa"
-        , File "pope_time.avi" "god bless"
-        , Folder "pics"
-            [ File "ape_throwing_up.jpg" "bleargh"
-            , File "watermelon_smash.gif" "smash!!"
-            , File "skull_man(scary).bmp" "Yikes!"
-            ]
-        , File "dijon_poupon.doc" "best mustard"
-        , Folder "programs"
-            [ File "fartwizard.exe" "10gotofart"
-            , File "owl_bandit.dmg" "mov eax, h00t"
-            , File "not_a_virus.exe" "really not a virus"
-            , Folder "source code"
-                [ File "best_hs_prog.hs" "main = print (fix error)"
-                , File "random.hs" "main = print 4"
-                ]
-            ]
-        ]
-
 
 data FSCrumb = FSCrumb Name [FSItem] [FSItem] deriving (Show)
 type FSZipper = (FSItem, [FSCrumb])
