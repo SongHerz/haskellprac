d2568f3529ff0dc89504149822470d6ed1c692ac
Hongzhi Song <dldz2002@gmail.com>
2014-02-16 23:42:18 +0800

    Add more examples for ch03

---
+++ real_world_haskell/ch03/GoodIndent.hs
@@ -0,0 +1,7 @@
+-- This is the leftmost column
+
+ -- it's fine for top-level declarations to start in any column...
+ firstGoodIndentation = 1
+
+ -- ...provided all subsquent declarations do, too!
+ secondGoodIndentation = 2
