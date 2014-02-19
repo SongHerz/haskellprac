330fb3a6a7c4e1ca4c90b45d14142c9a503d24f3
Hongzhi Song <dldz2002@gmail.com>
2014-02-19 23:55:24 +0800

    More examples from ch03

--- real_world_haskell/ch03/Tree.hs
+++ real_world_haskell/ch03/Tree.hs
@@ -2,6 +2,19 @@ data Tree a = Node a (Tree a) (Tree a)
             | Empty
             deriving (Show)
 
+leftNode (Node _ l _)   = Just l
+leftNode Empty          = Nothing
+
+rightNode (Node _ _ r)  = Just r
+rightNode Empty         = Nothing
+
+fromJust (Just n) = n
+
+nodesAreSame (Node a _ _) (Node b _ _)
+    | a == b     = Just a
+nodesAreSame _ _ = Nothing
+
+
 simpleTree = Node "parent" (Node "left child" Empty Empty)
                            (Node "right child" Empty Empty)
 
