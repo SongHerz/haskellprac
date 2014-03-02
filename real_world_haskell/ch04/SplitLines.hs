ce204614c1ed1544b4a80d92b726e6751f01a2de
Hongzhi Song <dldz2002@gmail.com>
2014-03-02 14:31:15 +0800

    Add split lines file

---
+++ real_world_haskell/ch04/SplitLines.hs
@@ -0,0 +1,12 @@
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest)      -> splitLines rest
+                ('\n':rest)      -> splitLines rest
+                []               -> []
+                _                -> error "Should never be here"
+
+isLineTerminator c = c == '\r' || c == '\n'
