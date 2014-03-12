7a4bcb73155afea5405e12b8f47d7c7cab4f84e3
Hongzhi Song <dldz2002@gmail.com>
2014-03-12 23:14:11 +0800

    Use either to handle asInt_foldl error

--- real_world_haskell/ch04/IntParse.hs
+++ real_world_haskell/ch04/IntParse.hs
@@ -40,6 +40,30 @@ asInt_foldl' xs
             | otherwise = error ('\'': x : "' is not a digit")
 
 
+type ErrorMessage = String
+
+asInt_foldl'' :: String -> Either ErrorMessage Int
+asInt_foldl'' [] = Left "String is empty"
+asInt_foldl'' xs
+    | head xs == '-' = negvalue (inner (tail xs))
+    | otherwise      = inner xs
+    where negvalue (Right v) = Right (negate v)
+          negvalue (Left m)  = Left m
+          inner [] = Left "No digit found"
+          inner xs = foldl' step (Right 0) xs
+          step (Left acc) x = Left acc
+          step (Right acc) x
+            | isDigit x = let newDigit = digitToInt x
+                              newAcc = acc * 10 + newDigit
+                          -- FIXME: I do not know, 
+                          -- if there are some proved more elegant solution 
+                          -- to tell overflow.
+                          in if (newAcc - newDigit) `div` 10 == acc
+                             then Right newAcc
+                             else Left "overflow"
+            | otherwise = Left ('\'': x : "' is not a digit")
+
+
 -- Obviously, foldl version is much cleaner better
 asInt_foldr :: String -> Int
 asInt_foldr xs = snd (foldr step (1,0) xs)
