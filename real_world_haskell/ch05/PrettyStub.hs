16a9797c42bc3977cbfdf87e8d639b5d77dd99bd
Hongzhi Song <dldz2002@gmail.com>
2014-03-29 21:50:59 +0800

    Add initial pretty-print framework

---
+++ real_world_haskell/ch05/PrettyStub.hs
@@ -0,0 +1,25 @@
+module PrettyStub where
+
+import SimpleJSON
+
+data Doc = ToBeDefined
+         deriving (Show)
+
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat = undefined
+
+fsep :: [Doc] -> Doc
+fsep xs = undefined
