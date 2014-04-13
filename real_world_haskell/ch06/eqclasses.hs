11d59a4ae0bd122c92662e78a64bfbb40bb465d4
Hongzhi Song <dldz2002@gmail.com>
2014-04-13 12:00:02 +0800

    Begin ch06

---
+++ real_world_haskell/ch06/eqclasses.hs
@@ -0,0 +1,34 @@
+class BasicEq a where
+    isEqual :: a -> a -> Bool
+
+instance BasicEq Bool where
+    isEqual True  True  = True
+    isEqual False False = True
+    isEqual _     _     = False
+
+
+class BasicEq2 a where
+    isEqual2    :: a -> a -> Bool
+    isNotEqual2 :: a -> a -> Bool
+
+
+class BasicEq3 a where
+    isEqual3 :: a -> a -> Bool
+    isEqual3 x y = not (isNotEqual3 x y)
+
+    isNotEqual3 :: a -> a -> Bool
+    isNotEqual3 x y = not (isEqual3 x y)
+
+-- When no isEqual3 or isNotEqual3 defined for a type instance,
+-- with default implementation from the type class,
+-- (isEqual3/isNotEqual3 Bool Bool) would run recursively endlessly
+-- until stack overflow.
+instance BasicEq3 Bool
+
+
+data Color = Red | Green | Blue
+instance BasicEq3 Color where
+    isEqual3 Red Red = True
+    isEqual3 Green Green = True
+    isEqual3 Blue Blue = True
+    isEqual3 _ _ = False
