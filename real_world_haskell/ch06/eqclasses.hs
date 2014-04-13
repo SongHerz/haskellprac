d339b58a9132fb42125f3fb240b0adf23beef3d5
Hongzhi Song <dldz2002@gmail.com>
2014-04-13 18:33:26 +0800

    Add read instance for data Color

--- real_world_haskell/ch06/eqclasses.hs
+++ real_world_haskell/ch06/eqclasses.hs
@@ -1,3 +1,6 @@
+import Data.List (isPrefixOf)
+import Data.Char (isSpace)
+
 class BasicEq a where
     isEqual :: a -> a -> Bool
 
@@ -32,3 +35,40 @@ instance BasicEq3 Color where
     isEqual3 Green Green = True
     isEqual3 Blue Blue = True
     isEqual3 _ _ = False
+
+instance Show Color where
+    show Red   = "Red"
+    show Green = "Green"
+    show Blue  = "Blue"
+
+-- The is the implementation from page 142, but (isPrefixOf) is used
+-- instead of (take)
+-- instance Read Color where
+--     -- readsPrec is the main function for parsing input
+--     readsPrec _ value =
+--         -- We pass tryParse a list of pairs. Each pair has a string
+--         -- and the desired return value. tryParse will try to match
+--         -- the input to one of these strings.
+--         tryParse [("Red", Red), ("Green", Green), ("Blue", Blue)]
+--         where tryParse [] = []  -- If there is nothing left to try, fail
+--               tryParse ((attempt, result):xs) =
+--                       -- Compare the start of the string to be parsed to the
+--                       -- text we are looking for.
+--                       if attempt `isPrefixOf` value
+--                          -- If we have a match, return the result and the
+--                          -- remaining input
+--                          then [(result, drop (length attempt) value)]
+--                          -- If we don't have a match, try the next pair
+--                          -- in the list of attempts.
+--                          else tryParse xs
+
+-- This is the enhanced version with dropWhile and preceding spaces trimmed
+-- At least, I think this version is much shorter and cleaner than the one
+-- given on the book.
+instance Read Color where
+    readsPrec _ value =
+        case dropWhile (not.(`isPrefixOf` trimmedValue).fst) attemptResultPairs of
+            (attempt, result) : _ -> [(result, drop (length attempt) trimmedValue)]
+            _                     -> []
+        where trimmedValue = dropWhile isSpace value
+              attemptResultPairs = [("Red", Red), ("Green", Green), ("Blue", Blue)]
