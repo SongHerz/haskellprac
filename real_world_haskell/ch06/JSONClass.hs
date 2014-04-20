f21976a8f5b8b509e9085e468317e4168f1c44c8
Hongzhi Song <dldz2002@gmail.com>
2014-04-20 18:09:53 +0800

    New JSON definition with JAry and JObj

--- real_world_haskell/ch06/JSONClass.hs
+++ real_world_haskell/ch06/JSONClass.hs
@@ -1,6 +1,31 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
-import SimpleJSON
+module JSONClass
+    (
+      JValue(..)
+    , JAry(..)
+    , JObj(..)
+    , toJValue
+    , fromJValue
+    ) where
+
+import Control.Arrow (second)
+
+newtype JAry a = JAry {
+        fromJAry :: [a]
+    } deriving (Eq, Ord, Show)
+
+newtype JObj a = JObj {
+        fromJObj :: [(String, a)]
+    } deriving (Eq, Ord, Show)
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject (JObj JValue)  -- was [(String, JValue)]
+            | JArray (JAry JValue)   -- was [JValue]
+              deriving (Eq, Ord, Show)
 
 type JSONError = String
 
@@ -38,10 +63,32 @@ instance JSON Double where
     toJValue = JNumber
     fromJValue = doubleToJValue id
 
-instance JSON [a] where
-    toJValue = undefined
-    fromJValue = undefined
+jaryFromJValue :: (JSON a) => JValue -> Either JSONError (JAry a)
+jaryFromJValue (JArray (JAry a)) =
+    whenRight JAry (mapEithers fromJValue a)
+jaryFromJValue _ = Left "not a JSON array"
 
-instance JSON (String, a) where
-    toJValue = undefined
-    fromJValue = undefined
+whenRight :: (b -> c) -> Either a b -> Either a c
+whenRight _ (Left err) = Left err
+whenRight f (Right a) = Right (f a)
+
+mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
+mapEithers f (x:xs) = case mapEithers f xs of
+                        Left err -> Left err
+                        Right ys -> case f x of
+                                      Left err -> Left err
+                                      Right y -> Right (y:ys)
+mapEithers _ _ = Right []
+
+jaryToJValue :: (JSON a) => JAry a -> JValue
+jaryToJValue = JArray . JAry . map toJValue . fromJAry
+
+instance (JSON a) => JSON (JAry a) where
+    toJValue = jaryToJValue
+    fromJValue = jaryFromJValue
+
+instance (JSON a) => JSON (JObj a) where
+    toJValue = JObject . JObj . map (second toJValue) . fromJObj 
+    fromJValue (JObject (JObj o)) = whenRight JObj (mapEithers unwrap o)
+        where unwrap (k, v) = whenRight ((,) k) (fromJValue v)
+    fromJValue _ = Left "not a JSON object"
