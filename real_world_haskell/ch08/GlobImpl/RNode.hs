1196965049cf53fbe51df72c962a99adf5a65aca
Hongzhi Song <dldz2002@gmail.com>
2014-10-05 11:28:38 +0800

    Initial range support

--- real_world_haskell/ch08/GlobImpl/RNode.hs
+++ real_world_haskell/ch08/GlobImpl/RNode.hs
@@ -36,12 +36,47 @@ preProcClass xs       = (   RClass { inclusive = not isExclusive,
                               xs' = if isExclusive then tail xs else xs
                               (remaining, charSet) = preProcClass' xs' [] 
 
--- From a string get char list of a character class
+{-
+ - From a string get char list of a character class
+ -}
 preProcClass' :: String -> CharSet -> (String, CharSet)
 preProcClass' (']':xs) cs    = (xs, cs)
+
+-- Range and escaped char support
+preProcClass' ('\\':'-':'-':e:xs) cs
+    | e /= ']'  = preProcClassWithRange '-' e xs cs
+    | otherwise = error "Unterminated character class range -1"
+
+preProcClass' ('\\':'-':xs) cs = (xs', '-':cs')
+                                 where (xs', cs') = preProcClass' xs cs
+preProcClass' ('\\':s:'-':'\\':e:xs) cs = preProcClassWithRange s e xs cs
+
+preProcClass' ('\\':s:'-':e:xs) cs
+    | e /= ']'  = preProcClassWithRange s e xs cs
+    | otherwise = error "Unterminated character class range 0"
+
+preProcClass' (s:'-':'\\':e:xs) cs = preProcClassWithRange s e xs cs
+
+preProcClass' (s:'-':e:xs) cs
+    | e /= ']'  = preProcClassWithRange s e xs cs 
+    | otherwise = error "Unterminated character class range 1"
+
+-- Single escaped char support
 preProcClass' ('\\':c:xs) cs = (xs', c:cs')
                                where (xs', cs') = preProcClass' xs cs
 preProcClass' (c:xs)   cs    = (xs', c:cs')
                                where (xs', cs') = preProcClass' xs cs
 preProcClass' []       _     = error "unterminated character class"
 
+
+-- Return a list of chars given inclusive range [start, end]
+classRange :: Char -> Char -> [Char]
+classRange start end
+    | start <= end  = [start .. end]
+    | otherwise     = [end .. start]
+
+
+preProcClassWithRange :: Char -> Char -> String -> CharSet -> (String, CharSet)
+preProcClassWithRange start end xs cs =
+    (xs', classRange start end ++ cs')
+    where (xs', cs') = preProcClass' xs cs
--- real_world_haskell/ch08/GlobImpl/RNode_Test.hs
+++ real_world_haskell/ch08/GlobImpl/RNode_Test.hs
@@ -37,7 +37,27 @@ tests' = [
     ("Exclusive character class with only '\\'", "", "[!\\\\]", [RClass {inclusive = False, chars = "\\"}]),
     ("Exclusive character class with ']' and '!' 0", "", "[!\\]\\!]", [RClass {inclusive = False, chars = "]!"}]),
     ("Exclusive character class with ']' and '!' 1", "", "[!a\\]!]", [RClass {inclusive = False, chars = "a]!"}]),
-    ("Exclusive character class with normal chars", "", "[!abc]", [RClass {inclusive = False, chars = "abc"}])
+    ("Exclusive character class with normal chars", "", "[!abc]", [RClass {inclusive = False, chars = "abc"}]),
+    -- Character range support
+    ("Inclusive with '-' 0", "", "[-]", [RClass {inclusive = True, chars = "-"}]),
+    ("Inclusive with '-' 1", "", "[-a]", [RClass {inclusive = True, chars = "-a"}]),
+    ("Inclusive with '-' 2", "", "[a\\-c]", [RClass {inclusive = True, chars = "a-c"}]),
+    ("Inclusive with '-' 3", "", "[-z]", [RClass {inclusive = True, chars = "-z"}]),
+    ("Inclusive with '-' 4", "", "[-az]", [RClass {inclusive = True, chars = "-az"}]),
+    ("Inclusive with '-' 5", "", "[a\\-cz]", [RClass {inclusive = True, chars = "a-cz"}]),
+    ("Inclusive normal range 0", "", "[a-c]", [RClass {inclusive = True, chars = "abc"}]),
+    ("Inclusive normal range 1", "", "[\\--0]", [RClass {inclusive = True, chars = "-./0"}]),
+    ("Inclusive normal range 2", "", "[\\a-\\c]", [RClass {inclusive = True, chars = "abc"}]),
+    ("Inclusive normal range 3", "", "[\\a-c]", [RClass {inclusive = True, chars = "abc"}]),
+    ("Inclusive normal range 4", "", "[a-\\c]", [RClass {inclusive = True, chars = "abc"}]),
+    ("Inclusive normal range 5", "", "[a-cz]", [RClass {inclusive = True, chars = "abcz"}]),
+    ("Inclusive normal range 6", "", "[\\--0z]", [RClass {inclusive = True, chars = "-./0z"}]),
+    ("Inclusive normal range 7", "", "[\\a-\\cz]", [RClass {inclusive = True, chars = "abcz"}]),
+    ("Inclusive normal range 8", "", "[\\a-cz]", [RClass {inclusive = True, chars = "abcz"}]),
+    ("Inclusive normal range 9", "", "[a-\\cz]", [RClass {inclusive = True, chars = "abcz"}]),
+    ("Inclusive range after range 0", "", "[a-\\cx-z]", [RClass {inclusive = True, chars = "abcxyz"}]),
+    ("Inclusive range , char, then range", "", "[a-\\cfx-z]", [RClass {inclusive = True, chars = "abcfxyz"}]),
+    ("Exclusive with '-' 0", "", "[!-]", [RClass {inclusive = False, chars = "-"}])
 
     ]
 
