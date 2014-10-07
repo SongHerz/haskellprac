9bc2591952b42fd906791308d12eaa19ba572f10
Hongzhi Song <dldz2002@gmail.com>
2014-10-07 10:45:48 +0800

    Add an important comment on how to interpret the code
    Actually, some code on lifting is not the same as that of chapter 9, as
    I think the style of chapter 9 is not readable for me, and it makes me
    confusing at the first glance.

--- real_world_haskell/ch09/BetterPredicate.hs
+++ real_world_haskell/ch09/BetterPredicate.hs
@@ -7,11 +7,13 @@ import System.IO (IOMode(..), hClose, hFileSize, openFile)
 
 import RecursiveContents (getRecursiveContents)
 
-type Predicate = FilePath       -- path to directory entry
-              -> Permissions    -- permissions
-              -> Maybe Integer  -- file size (Nothing if not file)
-              -> UTCTime        -- last modified
-              -> Bool
+type InfoP a = FilePath         -- path to directory entry
+            -> Permissions      -- permissions
+            -> Maybe Integer    -- file size (Nothing if not file)
+            -> UTCTime          -- last modified
+            -> a
+
+type Predicate = InfoP Bool
 
 getFileSize :: FilePath -> IO (Maybe Integer)
 
@@ -31,19 +33,80 @@ betterFind p path = getRecursiveContents path >>= filterM check
             return $ p name perms size modified
 
 
-simpleFileSize :: FilePath -> IO Integer
-
-simpleFileSize path = do
-    h <- openFile path ReadMode
-    size <- hFileSize h
-    hClose h
-    return size
 
 
-saferFileSize :: FilePath -> IO (Maybe Integer)
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
 
-saferFileSize path = handle ((\_ -> return Nothing) :: IOError -> IO (Maybe Integer)) $ do
-    h <- openFile path ReadMode
-    size <- hFileSize h
-    hClose h
-    return (Just size)
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing     _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k = \w x y z -> f w x y z == k
+
+{-
+ - Actually, "InfoP a -> a -> InfoP Bool" equals
+ - "InfoP a -> a -> (FilePath -> Permissions -> Maybe Integer -> UTCTime -> Bool)"
+ -
+ - Because a function type signature is right-associative,
+ - the above signature equals:
+ - "InfoP a -> a -> FilePath -> Permissions -> Maybe Integer -> UTCTime -> Bool"
+ -
+ - And we can write equalP' below.
+ -
+ - Here is parameters and typem mapping.
+ - "InfoP a -> a -> FilePath -> Permissions -> Maybe Integer -> UTCTime -> Bool"
+ -    f        k       w             x              y             z
+ -
+ - However, actually I prefer the equalP version, because I think it is more readable.
+ -
+ - Reference: http://stackoverflow.com/questions/4768453/type-signature-vs-function-equation-in-haskell
+ - See the last comment by 'Or When'
+ -
+ -}
+equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP' f k w x y z = f w x y z == k
+
+-- liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+-- liftP q f k w x y z = f w x y z `q` k
+--
+-- I prefer this form
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k = \w x y z -> f w x y z `q` k
+
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP  = liftP (<)
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g = \w x y z -> f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g = \w x y z -> f w x y z `q` g w x y z
+
+andP = liftP2 (&&)
+orP  = liftP2 (||)
+
+constP :: a -> InfoP a
+constP k = \_ _ _ _ -> k
+
+-- Rewrite liftP by liftP2
+liftP' :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP' q f k = liftP2 q f (constP k)
+
+
+myTest :: InfoP Bool
+myTest path _ (Just size) _ = takeExtension path == ".cpp" && size > 1024 * 128
+myTest _ _ _ _ = False
+
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath q = \path _ _ _ -> q path
+
+myTest2 :: InfoP Bool
+myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP`
+          (sizeP `greaterP` (1024 * 128))
+
+
