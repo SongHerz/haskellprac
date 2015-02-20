dba5f6ef4aa3d77daf1241b19200c27d27781e47
Hongzhi Song <dldz2002@gmail.com>
2015-02-20 11:46:29 +0800

    New iterators and local function type signatures

--- real_world_haskell/ch09/FoldDir.hs
+++ real_world_haskell/ch09/FoldDir.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module FoldDir where
 
-import System.FilePath((</>))
+import Data.Char (toLower)
+import System.FilePath(
+                      (</>)
+                      , takeFileName
+                      , takeExtension)
 
 import FSCommon (getUsefulContents, Info(..), getInfo, isDirectory)
 
@@ -11,14 +17,21 @@ data Iterate seed = Done     { unwrap :: seed }
 
 type Iterator seed = seed -> Info -> Iterate seed
 
-foldTree :: Iterator a -> a -> FilePath -> IO a
+foldTree :: forall a. Iterator a -> a -> FilePath -> IO a
 
 foldTree iter initSeed path = do
     endSeed <- fold initSeed path
     return $ unwrap endSeed
     where
+        -- Add func signature for better interpretation
+        -- Scoped type variable support is required, otherwise
+        -- the type var 'a' in this signature is not 'a' in foldTree
+        -- signature, and compiles with error.
+        fold :: a -> FilePath -> IO (Iterate a)
         fold seed subpath = getUsefulContents subpath >>= walk seed
 
+        -- Use the same type var 'a' as foldTree
+        walk :: a -> [FilePath] -> IO (Iterate a)
         walk seed (name:names) = do
             let path' = path </> name
             info <- getInfo path'
@@ -33,3 +46,25 @@ foldTree iter initSeed path = do
                             seed''          -> walk (unwrap seed'') names
                     | otherwise -> walk seed' names
         walk seed _ = return (Continue seed)
+
+
+atMostThreePictures :: Iterator [FilePath]
+
+atMostThreePictures paths info
+    | length paths == 3
+        = Done paths
+    | isDirectory info && takeFileName path == ".svn"
+        = Skip paths
+    | extension `elem` [".jpg", ".png"]
+        = Continue (path: paths)
+    | otherwise
+        = Continue paths
+  where extension = map toLower $ takeExtension path
+        path = infoPath info
+
+
+countDirectories :: Iterator Integer
+countDirectories count info =
+    Continue (if isDirectory info
+              then count + 1
+              else count)
