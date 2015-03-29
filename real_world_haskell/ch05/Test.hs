c94ada0cb1d32a1e2aff54b9e3f182e01707c537
Hongzhi Song <dldz2002@gmail.com>
2015-03-29 17:28:59 +0800

    Make runTests works for QuickCheck 2.6

--- real_world_haskell/ch05/Test.hs
+++ real_world_haskell/ch05/Test.hs
@@ -1,6 +1,8 @@
 -- This is created when reading Ch11
-import Test.QuickCheck
 import Prettify
+
+import Test.QuickCheck
+import Data.List (intersperse)
 import Control.Monad (liftM, liftM2)
 
 -- This is a better version without intermediate vars.
@@ -23,10 +25,43 @@ prop_text s = text s == if null s then Empty else Text s
 prop_line = line == Line
 prop_double d = double d == text (show d)
 
-main = do
-    verboseCheck ((\_ -> True) :: Doc -> Bool)
-    quickCheck prop_empty_id
-    quickCheck prop_char
-    quickCheck prop_text
-    quickCheck prop_line
-    quickCheck prop_double
+prop_hcat xs = hcat xs == glue xs
+    where
+        glue [] = empty
+        glue (d:ds) = d <> glue ds
+
+prop_punctuate s xs = punctuate s xs == combine (intersperse s xs)
+    where
+        combine [] = []
+        combine [x] = [x]
+
+        combine (x:Empty:ys) = x : combine ys
+        combine (Empty:y:ys) = y : combine ys
+        combine (x:y:ys) = x `Concat` y : combine ys
+
+
+args = Args
+    { replay = Nothing
+    , maxSuccess = 200
+    , maxDiscardRatio = 100
+    , maxSize = 100
+    , chatty = True
+    }
+
+runTests :: Args -> IO ()
+runTests args = do
+        f prop_empty_id "empty id"
+        f prop_char "char"
+        f prop_text "text"
+        f prop_line "line"
+        f prop_double "double"
+        f prop_hcat "hcat"
+        f prop_punctuate "punctuate"
+        return ()
+    where
+        f prop s = do
+            putStrLn s
+            quickCheckWithResult args prop
+
+main :: IO ()
+main = runTests args
