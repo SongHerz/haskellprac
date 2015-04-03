a316b95ef4adc60e4aab4c9c31c5b27f8461f7a7
Hongzhi Song <dldz2002@gmail.com>
2015-04-03 23:27:28 +0800

    Add some tables

--- real_world_haskell/ch12/Barcode.hs
+++ real_world_haskell/ch12/Barcode.hs
@@ -2,6 +2,8 @@
 -- For details of EAN-13 barcode.
 module EAN13 where
 
+import Control.Applicative ((<$>))
+
 -- http://www.gs1.org/check-digit-calculator
 -- 978354004934(0)
 -- Not the same as the book.
@@ -12,3 +14,44 @@ checkDigit ds = (10 - sum products) `mod` 10
 
 mapEveryOther :: (a -> a) -> [a] -> [a]
 mapEveryOther f = zipWith ($) (cycle [f, id])
+
+-- Encoding of digits
+leftOddList = [ "0001101"   -- 0
+              , "0011001"   -- 1
+              , "0010011"   -- 2
+              , "0111101"   -- 3
+              , "0100011"   -- 4
+              , "0110001"   -- 5
+              , "0101111"   -- 6
+              , "0111011"   -- 7
+              , "0110111"   -- 8
+              , "0001011"   -- 9
+              ]
+
+rightEvenList = map complement <$> leftOddList
+    where complement '0' = '1'
+          complement '1' = '0'
+
+leftEvenList = map reverse rightEvenList
+
+-- The parity list is for left 6 digits only.
+-- And the parity combinations of the left 6 digits determine the 1st digit
+-- of the bar code.
+-- '0' even parity.
+-- '1' odd parity.
+-- 
+-- In the wikipedia mentioned at the beginning of this module,
+-- This table is called "Structure of EAN-13"
+--
+--                 Parity      1st Digit
+leftParityList = [ "111111"     -- 0
+                 , "110100"     -- 1
+                 , "110010"     -- 2
+                 , "110001"     -- 3
+                 , "101100"     -- 4
+                 , "100110"     -- 5
+                 , "100011"     -- 6
+                 , "101010"     -- 7
+                 , "101001"     -- 8
+                 , "100101"     -- 9
+                 ]
