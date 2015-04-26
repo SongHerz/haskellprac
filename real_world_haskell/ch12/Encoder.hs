1292c8f956e9391790ce426e9630c47668874790
Hongzhi Song <dldz2002@gmail.com>
2015-04-26 13:22:12 +0800

    Prepare to separate Text/GUI show functionality

--- real_world_haskell/ch12/Encoder.hs
+++ real_world_haskell/ch12/Encoder.hs
@@ -1,5 +1,5 @@
 
-module Encoder (textEncode) where
+module Encoder (textEncode, guiEncode) where
 
 import qualified EAN13
 import Data.List (intercalate)
@@ -10,8 +10,23 @@ internalEncode xs
     | length xs == 12 = Right $ EAN13.encodeDigits xs
     | otherwise       = Left $ "Code length should be 12, but given code length is " ++ show (length xs)
 
+-- | Convert EAN13 code in [String] presentation to human readable form.
+toHumanReadable :: [String] -> String
+toHumanReadable = intercalate ", "
+
 
 textEncode :: [Int] -> String
 textEncode xs = case internalEncode xs of
-                    Right ys -> intercalate ", " ys
+                    Right ys -> toHumanReadable ys
                     Left err -> err
+
+guiEncode :: [Int] -> IO String
+guiEncode xs = do 
+    case internalEncode xs of
+        Right ys -> do 
+                       guiShow ys
+                       return $ toHumanReadable ys
+        Left err -> return err
+
+guiShow :: [String] -> IO ()
+guiShow _ = return ()
--- real_world_haskell/ch12/Main.hs
+++ real_world_haskell/ch12/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
+import System.Environment (getArgs)
 import qualified Encoder
 
 main = do
+    args <- getArgs
     line <- getLine
-    putStrLn $ Encoder.textEncode $ line2Digits line
+    showEncoding (not.null $ args) $ line2Digits line
+    where showEncoding sel = if sel then showGUIEncoding else showTextEncoding 
+          line2Digits :: String -> [Int]
+          line2Digits line = map read $ words line
 
-line2Digits :: String -> [Int]
-line2Digits line = map read $ words line
+showTextEncoding :: [Int] -> IO ()
+showTextEncoding xs = putStrLn $ Encoder.textEncode xs
+
+showGUIEncoding :: [Int] -> IO ()
+showGUIEncoding xs = do
+    msg <- Encoder.guiEncode xs
+    putStrLn msg
