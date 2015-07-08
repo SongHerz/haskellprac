1ae9c4114611daebdcf84f103adcf8b06adbadac
Hongzhi Song <dldz2002@gmail.com>
2015-07-08 18:57:56 +0800

    Show all digits in the window

--- real_world_haskell/ch12/Encoder.hs
+++ real_world_haskell/ch12/Encoder.hs
@@ -48,11 +48,11 @@ guiShow xs ys cs = do
 drawCode :: [Int] -> Int -> [String] -> (G.Picture, Float, Float)
 drawCode xs cs ys = (pic, totalWidth, guardH)
     where pic = G.translate (- totalWidth / 2) 0 $ G.pictures $ concat [
-                drawBitsDigit leftMostBits w bitH (head xs) digitH digitH leftMostBitsOffset bitYO
+                drawBitsDigitSeries [(leftMostBits, leftMostDigit)] w bitH digitW digitH leftMostBitsOffset bitYO
               , drawBits leftGuard w guardH leftGuardOffset guardYO
-              , drawBits leftBits w bitH leftBitsOffset bitYO
+              , drawBitsDigitSeries (zip leftBits leftDigits) w bitH digitW digitH leftBitsOffset bitYO
               , drawBits innerGuard w guardH innerGuardOffset guardYO
-              , drawBits rightBits w bitH rightBitsOffset bitYO
+              , drawBitsDigitSeries (zip rightBits rightDigits) w bitH digitW digitH rightBitsOffset bitYO
               , drawBits rightGuard w guardH rightGuardOffset guardYO
               ]
 
@@ -62,16 +62,18 @@ drawCode xs cs ys = (pic, totalWidth, guardH)
           guardH = bitH * 1.2
           guardYO = 0
           digitH = guardH - bitH
+          digitW = w * 7.0
 
           leftMostBits = "0000000"
           (leftGuard, leftBits, innerGuard, rightBits, rightGuard) = toBits ys
+          (leftMostDigit, leftDigits, rightDigits) = toDigits xs cs
 
           leftMostBitsOffset = 0.0
           leftGuardOffset = leftMostBitsOffset + w * fromIntegral (length leftMostBits)
           leftBitsOffset = leftGuardOffset + w * fromIntegral (length leftGuard)
-          innerGuardOffset = leftBitsOffset + w * fromIntegral (length leftBits)
+          innerGuardOffset = leftBitsOffset + w * fromIntegral (sum $ map length leftBits)
           rightBitsOffset = innerGuardOffset + w * fromIntegral (length innerGuard)
-          rightGuardOffset = rightBitsOffset + w * fromIntegral (length rightBits)
+          rightGuardOffset = rightBitsOffset + w * fromIntegral (sum $ map length rightBits)
           totalWidth = rightGuardOffset + w * fromIntegral (length rightGuard)
 
 -- | Draw a bit.
@@ -107,17 +109,43 @@ drawBits (c:xs) w h xo yo = drawBit c w h xo yo : drawBits xs w h (xo + w) yo
 -- y offset: yo
 drawBitsDigit :: String -> Float -> Float -> Int -> Float -> Float -> Float -> Float -> [G.Picture]
 drawBitsDigit xs bw bh d dw dh xo yo = digitPic : drawBits xs bw bh xo yo
-    where fontDefaultHeight = 100.0
-          fontZoomRatio = dh / fontDefaultHeight
-          digitPic = G.translate xo (yo - bh / 2 - dh) $ G.scale fontZoomRatio fontZoomRatio $ G.text [intToDigit d]
+    where fontDefaultWidth = 100.0
+          fontDefaultHeight = 100.0
+          fontZoomRatioW = dw / fontDefaultWidth
+          fontZoomRatioH = dh / fontDefaultHeight
+          digitPic = G.translate xo (yo - bh / 2 - dh) $ G.scale fontZoomRatioW fontZoomRatioH $ G.text [intToDigit d]
+
+-- | Draw bits digit pairs
+-- bit width: bw
+-- bit height: bh
+-- digit width: dw
+-- digit height: dh
+-- x offset: xo
+-- y offset: yo
+drawBitsDigitSeries :: [(String, Int)] -> Float -> Float -> Float -> Float -> Float -> Float -> [G.Picture]
+drawBitsDigitSeries [] _ _ _ _ _ _ = []
+drawBitsDigitSeries ((xs, d) : rest) bw bh dw dh xo yo =
+    drawBitsDigit xs bw bh d dw dh xo yo ++ others 
+    where others = drawBitsDigitSeries rest bw bh dw dh (xo + (bw * (fromIntegral $ length xs))) yo
 
 -- | Split bits
 -- Split a given EAN13 bar code to
 -- (leftGuard, leftBits, innerGuard, rightBits, rightGuard)
-toBits :: [String] -> (String, String, String, String, String)
+toBits :: [String] -> (String, [String], String, [String], String)
 toBits xs = map5Tuple concat (leftGuard, leftBits, innerGuard, rightBits, rightGuard)
     where (leftGuard, rest0) = splitAt 1 xs
           (leftBits, rest1) = splitAt 6 rest0
           (innerGuard, rest2) = splitAt 1 rest1
           (rightBits, rightGuard) = splitAt 6 rest2
-          map5Tuple f (a, b, c, d, e) = (f a, f b, f c, f d, f e)
+          map5Tuple f (a, b, c, d, e) = (f a, b, f c, d, f e)
+
+-- | Split digits
+-- Split given code and checksum to
+-- (leftMostDigit, leftDigits, rightDigits)
+-- _12Digits: 12 digits list 
+-- cs: the checksum digit
+toDigits :: [Int] -> Int -> (Int, [Int], [Int])
+toDigits _12Digits cs= (leftMostDigit, leftDigits, rightDigits)
+    where (leftMostDigit : _, rest0) = splitAt 1 _12Digits
+          (leftDigits, rest1) = splitAt 6 rest0
+          rightDigits = rest1 ++ [cs]
