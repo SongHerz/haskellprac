a0e1f2be45cfcdf8c354540e682eb0a87e34bb00
Hongzhi Song <dldz2002@gmail.com>
2015-08-06 18:27:00 +0800

    Code on testing
    Still contains errors

--- real_world_haskell/ch12/Tests.hs
+++ real_world_haskell/ch12/Tests.hs
@@ -3,8 +3,12 @@ module Tests where
 import Test.QuickCheck
 import EAN13
 import Data.Array (listArray)
+import Data.Char (digitToInt)
 
 encodeDigitsToPixmap :: [Int] -> Pixmap
+encodeDigitsToPixmap xs = listArray $ map digitToInt char_series
+    where (digits, _) = encodeDigits xs
+          char_series = "00" ++ (concat digits) ++ "00"
 
 prop_closeloop :: Bool
 prop_closeloop = True
