b548bee0174b9496cf4c762c6e41d49ba7d05617
Hongzhi Song <dldz2002@gmail.com>
2015-08-06 23:52:01 +0800

    Convert given 12 digits to Pixmap

--- real_world_haskell/ch12/Tests.hs
+++ real_world_haskell/ch12/Tests.hs
@@ -5,10 +5,19 @@ import EAN13
 import Data.Array (listArray)
 import Data.Char (digitToInt)
 
+-- Convert a digit char to RGB
+bitCharToRGB :: Char -> RGB
+bitCharToRGB '0' = (255, 255, 255)
+bitCharToRGB '1' = (0, 0, 0)
+bitCharToRGB x   = error $ "Unsupported character" ++ [x]
+
 encodeDigitsToPixmap :: [Int] -> Pixmap
-encodeDigitsToPixmap xs = listArray $ map digitToInt char_series
+encodeDigitsToPixmap xs = listArray ((0, 0), (w - 1, h - 1)) $ concat $ take h $ repeat row
     where (digits, _) = encodeDigits xs
           char_series = "00" ++ (concat digits) ++ "00"
+          row = map bitCharToRGB char_series
+          w = length row
+          h = w
 
 prop_closeloop :: Bool
 prop_closeloop = True
