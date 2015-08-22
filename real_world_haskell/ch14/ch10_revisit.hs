3e4dcdc2b70b1c7fb25ce1f94dcd666d57334b64
Hongzhi Song <dldz2002@gmail.com>
2015-08-22 11:33:51 +0800

    Write some actions

--- real_world_haskell/ch14/ch10_revisit.hs
+++ real_world_haskell/ch14/ch10_revisit.hs
@@ -3,15 +3,21 @@
 -- The key:
 -- 1. The next operation is in the scope of the operator function.
 -- 2. Pass state implicitly by functions.
+-- 3. Each function is an action, and ==> and ==& compose actions to a new
+-- action.
+-- 4. To get actual result from input, just apply an action to the input.
 -- 
 -- AHHH, I cheated, I referred code of ch10 for
 -- Consume a
 -- ==>
 -- ==&
 -- :-(
+-- But rest code is written by me :-)
 {-# LANGUAGE ScopedTypeVariables #-}
 
-data State = State { getState :: String }
+import Data.Char (isSpace, isDigit)
+
+data State = State { getString :: String }
 
 data Consume a = Consume { runConsume :: State -> Maybe (a, State) }
 
@@ -31,3 +37,45 @@ identity a = Consume $ \s -> Just (a, s)
 (==&) ca cb = ca ==> \_ -> cb
 
 
+skipSpaces :: Consume ()
+skipSpaces = Consume helper
+    where helper :: State -> Maybe ((), State)
+          helper s = let str = getString s
+                         (_, non_spcs) = span isSpace str
+                         in Just ((), s {getString = non_spcs})
+
+getInt :: Consume Int
+getInt = Consume helper
+    where helper :: State -> Maybe (Int, State)
+          helper s = let str = getString s
+                         (digits, rest) = span isDigit str
+                         in case digits of
+                                [] -> Nothing
+                                _  -> Just (read digits, s {getString = rest})
+
+getX :: Consume Char
+getX = Consume helper
+    where helper :: State -> Maybe (Char, State)
+          helper s = let str = getString s
+                         in case str of
+                                'x' : rest -> Just ('x', s {getString = rest})
+                                'X' : rest -> Just ('X', s {getString = rest})
+                                _ -> Nothing
+
+-- Parse a string like "123 x 456", return (123, 456)
+parseAction :: Consume (Int, Int)
+parseAction = skipSpaces ==&
+         getInt ==> \d0 -> skipSpaces ==&
+         getX ==> \x -> skipSpaces ==&
+         getInt ==> \d1 -> identity (d0, d1)
+
+parse :: String -> Maybe (Int, Int)
+parse s = case runConsume parseAction $ State s of
+              Nothing -> Nothing
+              Just (v, _) -> Just v
+
+r0 = parse "123"
+r1 = parse "123x456"
+r2 = parse "   456   X 324  "
+
+              
