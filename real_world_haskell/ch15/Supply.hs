9912a6f457aa25d6596290baa99e8ebaf9baeedd
Hongzhi Song <dldz2002@gmail.com>
2015-09-20 16:10:00 +0800

    Use GeneralizedNewtypeDeriving extension

--- real_world_haskell/ch15/Supply.hs
+++ real_world_haskell/ch15/Supply.hs
@@ -1,3 +1,6 @@
+-- With this extension, boilerplate code is not necessary.
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Supply
 (
   Supply
@@ -8,6 +11,11 @@ module Supply
 import Control.Monad.State
 
 newtype Supply s a = S (State [s] a)
+                     deriving (Monad)
+
+{-
+- There are boilerplate code that is not necessary by
+- GeneralizedNewtypeDeriving extension.
 
 unwrapS :: Supply s a -> State [s] a
 unwrapS (S s) = s
@@ -15,6 +23,7 @@ unwrapS (S s) = s
 instance Monad (Supply s) where
     s >>= m = S (unwrapS s >>= unwrapS . m)
     return = S . return
+-}
 
 next :: Supply s (Maybe s)
 next = S $ do
