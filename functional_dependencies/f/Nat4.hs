5704fade27c986f2e25d16899d6eca35ab4468c4
Hongzhi Song <dldz2002@gmail.com>
2015-10-04 22:40:41 +0800

    Add comments and derivations for Pow and Power examples

--- functional_dependencies/f/Nat4.hs
+++ functional_dependencies/f/Nat4.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-import Prelude hiding (odd, even)
+import Prelude hiding (odd, even, pred)
 
 data Zero
 
@@ -15,11 +15,9 @@ data True
         
 data False
 
-class Even n b | n -> b where
-    even :: n -> b
+class Even n b | n -> b where even :: n -> b
 
-class Odd n b | n -> b where
-    odd :: n -> b
+class Odd n b | n -> b where odd :: n -> b
 
 instance Even Zero True
 
@@ -29,12 +27,22 @@ instance Odd Zero False
 
 instance Even n b => Odd (Succ n) b
 
+{-
+WITH functional dependencies, the complete type can be inferred.
 
-class Add a b c | a b -> c where
-    add :: a -> b -> c
+*Main> :type even (undefined::Three)
+even (undefined::Three) :: False
+
+*Main> :type odd (undefined::Three)
+odd (undefined::Three) :: True
+-}
+
+
+class Add a b c | a b -> c where add :: a -> b -> c
 
 instance Add Zero b b 
 
+{- a + b = c => (a + 1) + b = c + 1 -}
 instance Add a b c => Add (Succ a) b (Succ c)
 
 {-
@@ -71,11 +79,11 @@ c = Succ (Succ (Succ Three)) = Succ (Succ (Succ (Succ (Succ (Succ Zero)))))
 -}
 
 
-class Mul a b c | a b -> c where
-    mul :: a -> b -> c
+class Mul a b c | a b -> c where mul :: a -> b -> c
 
 instance Mul Zero b Zero
 
+{- a * b = c, b + c = d => (a + 1) * b = a * b + b = c + b = d -}
 instance (Mul a b c, Add b c d) => Mul (Succ a) b d
 
 u = undefined
@@ -134,4 +142,104 @@ d = Succ (Succ (Succ c))
 => :type mul (u::Three) (u::Three)
 =   Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ c2))))))))
 =   Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero))))))))
+-}
+
+
+-- ---------------------
+-- Power Static Version
+-- ---------------------
+type One = Succ Zero
+
+class Pow a b c | a b -> c where pow :: a -> b -> c
+
+instance Pow a Zero One
+
+{- a^b = c, a * c = d => a^(b + 1) = (a^b) * a = c * a = d -}
+instance (Pow a b c, Mul a c d) => Pow a (Succ b) d
+
+{-
+*Main> :type pow (u::Succ (Succ Zero)) (u::Zero)
+pow (u::Succ (Succ Zero)) (u::Zero) :: One
+
+*Main> :type pow (u::Succ One) (u::One)
+pow (u::Succ One) (u::One) :: Succ (Succ Zero)
+
+*Main> :type pow (u::Succ One) (u::Succ One)
+pow (u::Succ One) (u::Succ One) :: Succ (Succ (Succ (Succ Zero)))
+-}
+
+
+-- -----------------------------------
+-- Power Static/Dynamic Mixed Version
+-- -----------------------------------
+
+{- Pred means predecessor -}
+class Pred a b | a -> b where pred :: a -> b
+
+instance Pred (Succ n) n
+
+
+class Power n where
+    power :: Int -> n -> Int
+
+instance Power Zero where
+    power _ _ = 1
+
+instance Power n => Power (Succ n) where
+    power x n = x * power x (pred n)
+
+{-
+=================
+power x (u::Zero)
+=================
+power x (u::Zero)
+belongs to instance Power Zero.
+Thus, power x (u::Zero) = 1
+
+======================
+power x (u::Succ Zero)
+======================
+power x (u::Succ Zero)
+belongs to instance Power n => Power (Succ n)
+Thus, power x (u::Succ Zero) = x * power x (pred (u::Succ Zero))
+
+pred (u::Succ Zero)
+belongs to instance (Succ Zero) Zero.
+Thus, the type of (pred (u::Succ Zero)) is Zero. 
+And (power x (pred (u::Succ Zero))) belongs to instance Power Zero => Power (Succ Zero)
+And (power x (pred (u::Succ Zero)) = 1)
+And power x (u::Succ Zero) = x * 1 = x
+
+=====================
+power x (u::Succ One)
+=====================
+power x (u::Succ One)
+belongs to instance Power n => Power (Succ n)
+Thus, power x (u::Succ One) = x * power x (pred (u::Succ One))
+The type of (pred (u::Succ One)) is One,
+and (power x (pred (u::Succ One))) belongs instance Power n => Power (Succ n)
+Thus
+power x (pred (u::Succ One)) = x * power x (pred (pred (u::Succ One)))
+
+As type of (pred (u::Succ One)) is One/Succ Zero,
+type of (pred (pred (u::Succ One))) is type of (pred some_val::(Succ Zero)).
+
+Type of (pred some_val::(Succ Zero)) is Zero,
+and (power x (pred some_val::(Succ Zero))) belongs to instance Power Zero => Power (Succ Zero)
+And (power x (pred some_val::(Succ Zero)) = 1)
+And (power x (pred (pred (u::Succ One))) = 1)
+And (power x (pred (u::Succ One)) = x * power x (pred (pred (u::Succ One)) = x)
+And (power x (u::Succ One) = x * power x (pred (u::Succ One)) = x * x)
+
+==========
+Conclution
+==========
+power x (u::Natural) = x ^ Natural
+-}
+
+{-
+As shown above (mul (u::Three) (u::Three) = Nine)
+
+*Main> power 2 (mul (u::Three) (u::Three))
+512
 -}
